f877b265ff819b0f6eaa9fd12a16218f2ab454c1
mbskot2 <mbskot@gmail.com>
2014-03-30 23:16:16 +0200

    Doplnenie deterministických generátorov.

--- src/sk/uniza/fri/II008/generators/DiscreteDeterministicGenerator.java
+++ src/sk/uniza/fri/II008/generators/DiscreteDeterministicGenerator.java
@@ -1,33 +1,17 @@
 package sk.uniza.fri.II008.generators;
 
-import java.util.Random;
-
-public class DiscreteGenerator implements IDiscreteGenerator
+public class DiscreteDeterministicGenerator implements IDiscreteGenerator
 {
-	private final Random random;
-	private int min, max;
+	private final int value;
 
-	public DiscreteGenerator(Random random, int max)
+	public DiscreteDeterministicGenerator(int value)
 	{
-		this(random, 0, max);
-	}
-
-	public DiscreteGenerator(Random random, int min, int max)
-	{
-		this.random = random;
-
-		if (min >= max)
-		{
-			throw new IllegalArgumentException("Min value must be lower than max value.");
-		}
-
-		this.min = min;
-		this.max = max;
+		this.value = value;
 	}
 
 	@Override
 	public int nextValue()
 	{
-		return min + random.nextInt(max - min);
+		return value;
 	}
 }
